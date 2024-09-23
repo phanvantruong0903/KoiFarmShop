19b2187c60123c27c4d24cede0e3bb68d257f42e
zunguyen1505@gmail.com <zunguyen1505@gmail.com>
2024-09-23 10:38:31 +0700

    Vu Nguyen update full function chua custom lai

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    devSourcemap: true, //debug báo lỗi ở dòng nào cụ thể
+  },
+  server: {
+    port: 3000,
+  },
+});
