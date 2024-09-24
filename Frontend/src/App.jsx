c01ce4eae124ba8de3cd69dc00f941d360ae385c
An <tuanan298@icloud.com>
2024-09-24 17:24:08 +0700

    chuyển từ sử dụng route và routes sang createBroswerRouter cho giống với nguyên

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,19 +1,73 @@
-import { lazy, Suspense } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Suspense,lazy } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Spinner from "./An/Components/Spinner";
 import DashBoard from "./An/Pages/DashBoard";
+import Home from "./Home";
+import Login from "./Login";
+
+
+const Staff = lazy(() => import('../src/An/Pages/Staff'));
+const Manager = lazy(() => import('../src/An/Pages/Manager'));
+const StaffLayOut = lazy(() => import("../src/An/Layouts/StaffLayOut"));
+const ManagerLayOut = lazy(() => import("../src/An/Layouts/ManagerLayOut"));
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />, // Nguyen route setup tam thoi de v
+  },
+  {
+    path: "/login/oauth",
+    element: <Login />, // Nguyen route setup tam thoi de v
+  },
+  {
+    path: "/DashBoard",
+    element: <DashBoard />, //  DashBoard component
+    children: [
+      {
+        path: "staff",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <StaffLayOut />
+          </Suspense>
+        ),
+        children: [
+          {
+            index: true, // Default route for "/DashBoard/staff"
+            element: (
+              <Suspense fallback={<Spinner />}>
+                <Staff />
+              </Suspense>
+            ),
+          },
+        ],
+      },
+      {
+        path: "manager",
+        element: (
+          <Suspense fallback={<Spinner />}>
+            <ManagerLayOut />
+          </Suspense>
+        ),
+        children: [
+          {
+            index: true, // Default route for "/DashBoard/manager"
+            element: (
+              <Suspense fallback={<Spinner />}>
+                <Manager />
+              </Suspense>
+            ),
+          },
+        ],
+      },
+    ],
+  },
+]);
+
 function App() {
-
-
-  return (
-      <Suspense fallback={<Spinner />}>
-        <Routes>
-          <Route path="/DashBoard/*" element={<DashBoard />} />
-        </Routes>
-      </Suspense>
-
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
