98b7f9100d2fc3870efdef594df07354a6363a0a
zunguyen1505@gmail.com <zunguyen1505@gmail.com>
2024-10-16 15:11:46 +0700

    Vu Nguyen: Cập nhật lại groupKoiSchema và invoiceSchema

--- Backend/src/models/schemas/GroupKoi.schema.js
+++ Backend/src/models/schemas/GroupKoi.schema.js
@@ -2,10 +2,12 @@ import { ObjectId } from 'mongodb'
 
 export default class GroupKoiSchema {
   _id = new ObjectId()
+  SupplierID = ''
   GroupKoiCategoryID = ''
 
   constructor(groupKoi) {
     this._id = groupKoi?._id ?? new ObjectId() // tự tạo id
+    this.SupplierID = groupKoi.SupplierID || ''
     this.GroupKoiCategoryID = groupKoi.GroupKoiCategoryID || ''
   }
 }
--- Backend/src/models/schemas/Invoice.schema.js
+++ Backend/src/models/schemas/Invoice.schema.js
@@ -2,7 +2,6 @@ import { ObjectId } from 'mongodb'
 
 export default class InvoiceSchema {
   _id = new ObjectId()
-  SupplierID = ''
   GroupKoiIDInvoice = ''
   Quantity = ''
   InvoiceDate = new Date()
@@ -12,7 +11,6 @@ export default class InvoiceSchema {
   constructor(invoice) {
     const date = new Date()
     this._id = invoice?._id ?? new ObjectId() // tự tạo id
-    this.SupplierID = invoice.SupplierID || ''
     this.GroupKoiIDInvoice = invoice.GroupKoiIDInvoice || ''
     this.Quantity = invoice.Quantity || ''
     this.InvoiceDate = invoice.InvoiceDate || date
